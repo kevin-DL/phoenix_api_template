57dae2bee3ecc19a0043b80a1b92a2b11e0f78a6
Kevin ANATOLE <kevanatole@gmail.com>
2023-02-16 23:54:35 +0000

    Added default endpoint

--- lib/phoenix_api_template_web/router.ex
+++ lib/phoenix_api_template_web/router.ex
@@ -2,10 +2,12 @@ defmodule PhoenixApiTemplateWeb.Router do
   use PhoenixApiTemplateWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", PhoenixApiTemplateWeb do
-    pipe_through :api
+    pipe_through(:api)
+
+    get("/", DefaultController, :index)
   end
 end
