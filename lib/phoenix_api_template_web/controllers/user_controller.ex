ff8a0e6260d9b251a27a8c31a6baf81a988764fc
Kevin ANATOLE <kevanatole@gmail.com>
2023-02-18 10:50:52 +0000

    Login

--- lib/phoenix_api_template_web/controllers/user_controller.ex
+++ lib/phoenix_api_template_web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule PhoenixApiTemplateWeb.UserController do
   use PhoenixApiTemplateWeb, :controller
 
+  alias PhoenixApiTemplateWeb.Auth.ErrorResponse.Unauthorized
   alias PhoenixApiTemplateWeb.Auth.Guardian
   alias PhoenixApiTemplate.Accounts
   alias PhoenixApiTemplate.Accounts.User
@@ -21,6 +22,18 @@ defmodule PhoenixApiTemplateWeb.UserController do
       conn
       |> put_status(:created)
       |> render("user_token.json", user: user, token: token)
+    end
+  end
+
+  def sign_in(conn, %{"email" => email, "password" => password}) do
+    case Guardian.authenticate(email, password) do
+      {:ok, user, token} ->
+        conn
+        |> put_status(:ok)
+        |> render("user_token.json", %{user: user, token: token})
+
+      {:error, :unauthorized} ->
+        raise Unauthorized, message: "Invalid credentials"
     end
   end
 
--- lib/phoenix_api_template_web/router.ex
+++ lib/phoenix_api_template_web/router.ex
@@ -1,5 +1,18 @@
 defmodule PhoenixApiTemplateWeb.Router do
   use PhoenixApiTemplateWeb, :router
+  use Plug.ErrorHandler
+
+  defp handle_errors(conn, %{reason: %Phoenix.Router.NoRouteError{message: message}}) do
+    conn
+    |> json(%{errors: message})
+    |> halt()
+  end
+
+  defp handle_errors(conn, %{reason: %{message: message}}) do
+    conn
+    |> json(%{errors: message})
+    |> halt()
+  end
 
   pipeline :api do
     plug(:accepts, ["json"])
@@ -10,5 +23,6 @@ defmodule PhoenixApiTemplateWeb.Router do
 
     get("/", DefaultController, :index)
     post("/register", UserController, :create)
+    post("/login", UserController, :sign_in)
   end
 end
